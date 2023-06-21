96bb5b033dcef277c22aefb06b0a99a4d6f41c24
James Tennant <jtennant@ucsc.edu>
2023-06-21 14:20:23 -0700

    Simplify scene

--- Client/src/components/controls.tsx
+++ Client/src/components/controls.tsx
@@ -2,8 +2,16 @@ import { Text, SafeAreaView } from 'react-native';
 
 export default function Controls() {
   return (
-    <SafeAreaView>
-      <Text>Hello, Controls!</Text>
+    <SafeAreaView style={{ backgroundColor: '#423' }}>
+      <Text
+        style={{
+          padding: 30,
+          fontWeight: '700',
+          fontSize: 24,
+          color: '#fff',
+        }}>
+        Hello, Controls!
+      </Text>
     </SafeAreaView>
   );
 }
