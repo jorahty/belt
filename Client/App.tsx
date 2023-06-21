c3a7cff226e5c9646ff104d991918cf61f7e817f
James Tennant <jtennant@ucsc.edu>
2023-06-21 13:31:54 -0700

    Refactor into Canvas and Controls

--- Client/App.tsx
+++ Client/App.tsx
@@ -1,85 +1,15 @@
-import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
-import { Renderer, TextureLoader } from 'expo-three';
-import { useEffect } from 'react';
-import {
-  AmbientLight,
-  BoxGeometry,
-  Fog,
-  GridHelper,
-  Mesh,
-  MeshStandardMaterial,
-  PerspectiveCamera,
-  PointLight,
-  Scene,
-  SpotLight,
-} from 'three';
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import Canvas from './src/components/canvas';
+import Controls from './src/components/controls';
 
 export default function App() {
-  let timeout: number;
-
-  useEffect(() => {
-    // Clear the animation loop when the component unmounts
-    return () => clearTimeout(timeout);
-  }, []);
-
-  const onContextCreate = async (gl: ExpoWebGLRenderingContext) => {
-    const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
-    const sceneColor = 0x334466;
-
-    // Create a WebGLRenderer without a DOM element
-    const renderer = new Renderer({ gl });
-    renderer.setSize(width, height);
-    renderer.setClearColor(sceneColor);
-
-    const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
-    camera.position.set(2, 5, 5);
-
-    const scene = new Scene();
-    scene.fog = new Fog(sceneColor, 1, 10000);
-    scene.add(new GridHelper(10, 10));
-
-    const ambientLight = new AmbientLight(0x101010);
-    scene.add(ambientLight);
-
-    const pointLight = new PointLight(0xffffff, 2, 1000, 1);
-    pointLight.position.set(0, 200, 200);
-    scene.add(pointLight);
-
-    const spotLight = new SpotLight(0xffffff, 0.5);
-    spotLight.position.set(0, 500, 100);
-    spotLight.lookAt(scene.position);
-    scene.add(spotLight);
-
-    const cube = new IconMesh();
-    scene.add(cube);
-
-    camera.lookAt(cube.position);
-
-    function update() {
-      cube.rotation.y += 0.05;
-      cube.rotation.x += 0.025;
-    }
-
-    // Setup an animation loop
-    const render = () => {
-      timeout = requestAnimationFrame(render);
-      update();
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
-    render();
-  };
-
-  return <GLView style={{ flex: 1 }} onContextCreate={onContextCreate} />;
-}
-
-class IconMesh extends Mesh {
-  constructor() {
-    super(
-      new BoxGeometry(1.0, 1.0, 1.0),
-      new MeshStandardMaterial({
-        map: new TextureLoader().load(require('./assets/favicon.png')),
-      })
-    );
-  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Canvas />
+      <Controls />
+      <StatusBar hidden />
+    </View>
+  );
 }
---
+++ Client/src/components/controls.tsx
@@ -0,0 +1,9 @@
+import { Text, SafeAreaView } from 'react-native';
+
+export default function Controls() {
+  return (
+    <SafeAreaView>
+      <Text>Hello, Controls!</Text>
+    </SafeAreaView>
+  );
+}
