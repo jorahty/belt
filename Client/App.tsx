020921810a11a76de0c653f0b062b32cd6b300b5
James Tennant <jtennant@ucsc.edu>
2023-06-22 16:46:16 -0700

    Remove debug

--- Client/App.tsx
+++ Client/App.tsx
@@ -2,14 +2,12 @@ import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import Canvas from './src/components/canvas';
-import Debug from './src/components/debug';
 import Controls from './src/components/controls/controls';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
       <Canvas />
-      <Debug />
       <Controls />
       <StatusBar hidden />
     </View>
